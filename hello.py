79b78ddc31ce3eedc1a33d3f3fad5dab2e92c5c9
morristech <madguy69@gmail.com>
2020-06-30 22:47:15 -0300

    second python command but incomplete

--- hello.py
+++ hello.py
@@ -6,4 +6,17 @@ b = 7
 operation = multiply
 print(operation(a, b))
 
+def add(x, y):
+    return x + y
+
+def do_twice(func(x, y):
+    return func(func(x, y), func(x, y))
+
+    a = 5
+    b = 10
+
+    print(do_twice(add, a, b))
+    
+
+
 
